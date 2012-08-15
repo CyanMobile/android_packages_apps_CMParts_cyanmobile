1bfa5e25b2d5d36100a6645e085d494714003b39
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2012-08-15 22:51:25 +0700

    make softbutton dynamic

--- src/com/cyanogenmod/cmparts/activities/TabletSoftButtonsActivity.java
+++ src/com/cyanogenmod/cmparts/activities/TabletSoftButtonsActivity.java
@@ -19,6 +19,7 @@ package com.cyanogenmod.cmparts.activities;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
@@ -28,17 +29,17 @@ import android.provider.Settings;
 
 import com.cyanogenmod.cmparts.R;
 
-public class TabletSoftButtonsActivity extends PreferenceActivity {
+public class TabletSoftButtonsActivity extends PreferenceActivity implements OnPreferenceChangeListener{
     private static final String PREF_SOFT_BUTTONS_HOME = "pref_soft_buttons_home";
     private static final String PREF_SOFT_BUTTONS_MENU = "pref_soft_buttons_menu";
     private static final String PREF_SOFT_BUTTONS_BACK = "pref_soft_buttons_back";
     private static final String PREF_SOFT_BUTTONS_SEARCH = "pref_soft_buttons_search";
     private static final String PREF_SOFT_BUTTONS_QUICK_NA = "pref_soft_buttons_quick_na";
 
-    private CheckBoxPreference mSoftButtonsHome;
-    private CheckBoxPreference mSoftButtonsMenu;
-    private CheckBoxPreference mSoftButtonsBack;
-    private CheckBoxPreference mSoftButtonsSearch;
+    private ListPreference mSoftButtonsHome;
+    private ListPreference mSoftButtonsMenu;
+    private ListPreference mSoftButtonsBack;
+    private ListPreference mSoftButtonsSearch;
     private CheckBoxPreference mSoftButtonsQuickNa;
 
     @Override
@@ -50,26 +51,34 @@ public class TabletSoftButtonsActivity extends PreferenceActivity {
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        mSoftButtonsHome = (CheckBoxPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_HOME);
-        mSoftButtonsMenu = (CheckBoxPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_MENU);
-        mSoftButtonsBack = (CheckBoxPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_BACK);
-        mSoftButtonsSearch = (CheckBoxPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_SEARCH);
+        int ButtonsHomePref = Settings.System.getInt(getContentResolver(),
+                Settings.System.SOFT_BUTTON_SHOW_HOME, 1);
+        mSoftButtonsHome = (ListPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_HOME);
+        mSoftButtonsHome.setValue(String.valueOf(ButtonsHomePref));
+        mSoftButtonsHome.setOnPreferenceChangeListener(this);
+
+        int ButtonsMenuPref = Settings.System.getInt(getContentResolver(),
+                Settings.System.SOFT_BUTTON_SHOW_MENU, 4);
+        mSoftButtonsMenu = (ListPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_MENU);
+        mSoftButtonsMenu.setValue(String.valueOf(ButtonsMenuPref));
+        mSoftButtonsMenu.setOnPreferenceChangeListener(this);
+
+        int ButtonsBackPref = Settings.System.getInt(getContentResolver(),
+                Settings.System.SOFT_BUTTON_SHOW_BACK, 2);
+        mSoftButtonsBack = (ListPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_BACK);
+        mSoftButtonsBack.setValue(String.valueOf(ButtonsBackPref));
+        mSoftButtonsBack.setOnPreferenceChangeListener(this);
+
+        int ButtonsSearchPref = Settings.System.getInt(getContentResolver(),
+                Settings.System.SOFT_BUTTON_SHOW_SEARCH, 3);
+        mSoftButtonsSearch = (ListPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_SEARCH);
+        mSoftButtonsSearch.setValue(String.valueOf(ButtonsSearchPref));
+        mSoftButtonsSearch.setOnPreferenceChangeListener(this);
+
         mSoftButtonsQuickNa = (CheckBoxPreference) prefSet.findPreference(PREF_SOFT_BUTTONS_QUICK_NA);
 
         int defValue;
 
-        defValue=CmSystem.getDefaultBool(getBaseContext(), CmSystem.CM_DEFAULT_SHOW_SOFT_HOME)==true ? 1 : 0;
-        mSoftButtonsHome.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.SOFT_BUTTON_SHOW_HOME, defValue) == 1));
-        defValue=CmSystem.getDefaultBool(getBaseContext(), CmSystem.CM_DEFAULT_SHOW_SOFT_MENU)==true ? 1 : 0;
-        mSoftButtonsMenu.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.SOFT_BUTTON_SHOW_MENU, defValue) == 1));
-        defValue=CmSystem.getDefaultBool(getBaseContext(), CmSystem.CM_DEFAULT_SHOW_SOFT_BACK)==true ? 1 : 0;
-        mSoftButtonsBack.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.SOFT_BUTTON_SHOW_BACK, defValue) == 1));
-        defValue=CmSystem.getDefaultBool(getBaseContext(), CmSystem.CM_DEFAULT_SHOW_SOFT_SEARCH)==true ? 1 : 0;
-        mSoftButtonsSearch.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.SOFT_BUTTON_SHOW_SEARCH, defValue) == 1));
         defValue=CmSystem.getDefaultBool(getBaseContext(), CmSystem.CM_DEFAULT_SHOW_SOFT_QUICK_NA)==true ? 1 : 0;
         mSoftButtonsQuickNa.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.SOFT_BUTTON_SHOW_QUICK_NA, defValue) == 1));
@@ -79,33 +88,40 @@ public class TabletSoftButtonsActivity extends PreferenceActivity {
         boolean value;
 
         /* Preference Screens */
-        if (preference == mSoftButtonsHome) {
-            value = mSoftButtonsHome.isChecked();
-            Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_HOME,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mSoftButtonsMenu) {
-            value = mSoftButtonsMenu.isChecked();
-            Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_MENU,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mSoftButtonsBack) {
-            value = mSoftButtonsBack.isChecked();
-            Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_BACK,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mSoftButtonsSearch) {
-            value = mSoftButtonsSearch.isChecked();
-            Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_SEARCH,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mSoftButtonsQuickNa) {
+        if (preference == mSoftButtonsQuickNa) {
             value = mSoftButtonsQuickNa.isChecked();
             Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_QUICK_NA,
                     value ? 1 : 0);
             return true;
         }
 
+        return false;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+        /* Preference Screens */
+        if (preference == mSoftButtonsHome) {
+            int ButtonsHomePref = Integer.parseInt(String.valueOf(newValue));
+            Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_HOME,
+                          ButtonsHomePref);
+            return true;
+        } else if (preference == mSoftButtonsMenu) {
+            int ButtonsMenuPref = Integer.parseInt(String.valueOf(newValue));
+            Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_MENU,
+                          ButtonsMenuPref);
+            return true;
+        } else if (preference == mSoftButtonsBack) {
+            int ButtonsBackPref = Integer.parseInt(String.valueOf(newValue));
+            Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_BACK,
+                          ButtonsBackPref);
+            return true;
+        } else if (preference == mSoftButtonsSearch) {
+            int ButtonsSearchPref = Integer.parseInt(String.valueOf(newValue));
+            Settings.System.putInt(getContentResolver(), Settings.System.SOFT_BUTTON_SHOW_SEARCH,
+                          ButtonsSearchPref);
+            return true;
+        }
         return false;
     }
 }
