17656bfd873f00f58166e1f0c908f037d239f81b
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2012-09-24 00:48:32 +0700

    add support choose delay time for screen on animation

--- src/com/cyanogenmod/cmparts/activities/DisplayActivity.java
+++ src/com/cyanogenmod/cmparts/activities/DisplayActivity.java
@@ -85,6 +85,8 @@ public class DisplayActivity extends PreferenceActivity implements OnPreferenceC
 
     private static final String ELECTRON_BEAM_ANIMATION_OFF = "electron_beam_animation_off";
 
+    private static final String ELECTRON_BEAM_ANIMATION_ON_DELAY = "electron_beam_animation_on_delay";
+
     private static final String ROTATION_ANIMATION_PREF = "pref_rotation_animation";
 
     private static final String LCD_DENSITY_PREF = "lcd_density";
@@ -123,9 +125,8 @@ public class DisplayActivity extends PreferenceActivity implements OnPreferenceC
     private static final int MSG_DATA_CLEARED = 500;
 
     private CheckBoxPreference mElectronBeamAnimationOn;
-
     private CheckBoxPreference mElectronBeamAnimationOff;
-
+    private ListPreference mElectronBeamAnimationOnPref;
     private CheckBoxPreference mRotationAnimationPref;
 
     private CheckBoxPreference mUseBraviaPref;
@@ -191,6 +192,9 @@ public class DisplayActivity extends PreferenceActivity implements OnPreferenceC
         /* Electron Beam control */
         mElectronBeamAnimationOn = (CheckBoxPreference)prefSet.findPreference(ELECTRON_BEAM_ANIMATION_ON);
         mElectronBeamAnimationOff = (CheckBoxPreference)prefSet.findPreference(ELECTRON_BEAM_ANIMATION_OFF);
+        int electronBeamAnimationOnPref = Settings.System.getInt(getContentResolver(),
+                Settings.System.ELECTRON_BEAM_ANIMATION_ON_DELAY, 100);
+        mElectronBeamAnimationOnPref = (ListPreference) prefSet.findPreference(ELECTRON_BEAM_ANIMATION_ON_DELAY);
         if (getResources().getBoolean(com.android.internal.R.bool.config_enableScreenAnimation)) {
             mElectronBeamAnimationOn.setChecked(Settings.System.getInt(getContentResolver(),
                     Settings.System.ELECTRON_BEAM_ANIMATION_ON,
@@ -198,10 +202,14 @@ public class DisplayActivity extends PreferenceActivity implements OnPreferenceC
             mElectronBeamAnimationOff.setChecked(Settings.System.getInt(getContentResolver(),
                     Settings.System.ELECTRON_BEAM_ANIMATION_OFF,
                     getResources().getBoolean(com.android.internal.R.bool.config_enableScreenOffAnimation) ? 1 : 0) == 1);
+            mElectronBeamAnimationOnPref.setValue(String.valueOf(electronBeamAnimationOnPref));
+            mElectronBeamAnimationOnPref.setOnPreferenceChangeListener(this);
         } else {
             /* Hide Electron Beam controls if disabled */
             ((PreferenceCategory) prefSet.findPreference(GENERAL_CATEGORY))
                 .removePreference(mElectronBeamAnimationOn);
+            ((PreferenceCategory) prefSet.findPreference(GENERAL_CATEGORY))
+                .removePreference(mElectronBeamAnimationOnPref);
             ((PreferenceCategory) prefSet.findPreference(GENERAL_CATEGORY))
                 .removePreference(mElectronBeamAnimationOff);
         }
@@ -392,6 +400,11 @@ public class DisplayActivity extends PreferenceActivity implements OnPreferenceC
             return true;
         } else if (preference == mTransitionAnimationsPref) {
             writeAnimationPreference(1, newValue);
+            return true;
+        } else if (preference == mElectronBeamAnimationOnPref) {
+            int electronBeamAnimation = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.ELECTRON_BEAM_ANIMATION_ON_DELAY,
+                    electronBeamAnimation);
             return true;
         }
         return false;
