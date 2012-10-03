6cf34d9e5c62805fea51a2d5036df4e93ee0ad55
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2012-10-03 20:38:09 +0700

    Apply selected render effect on boot. (Maniac103)

--- src/com/cyanogenmod/cmparts/activities/UIActivity.java
+++ src/com/cyanogenmod/cmparts/activities/UIActivity.java
@@ -21,10 +21,6 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.IBinder;
-import android.os.Parcel;
-import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -43,6 +39,7 @@ import java.io.File;
 import java.io.IOException;
 
 import com.cyanogenmod.cmparts.R;
+import com.cyanogenmod.cmparts.utils.SurfaceFlingerUtils;
 
 public class UIActivity extends PreferenceActivity implements OnPreferenceChangeListener {
 
@@ -84,7 +81,7 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
 
     private static final String PINCH_REFLOW_PREF = "pref_pinch_reflow";
 
-    private static final String RENDER_EFFECT_PREF = "pref_render_effect";
+    public static final String RENDER_EFFECT_PREF = "pref_render_effect";
 
     private static final String SHARE_SCREENSHOT_PREF = "pref_share_screenshot";
 
@@ -180,13 +177,18 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
 
         mRenderEffectPref = (ListPreference) prefSet.findPreference(RENDER_EFFECT_PREF);
         mRenderEffectPref.setOnPreferenceChangeListener(this);
-        updateFlingerOptions();
 
         /* Share Screenshot */
         mShareScreenshotPref = (CheckBoxPreference) prefSet.findPreference(SHARE_SCREENSHOT_PREF);
         mShareScreenshotPref.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.SHARE_SCREENSHOT, 0) == 1);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mRenderEffectPref.setValue(String.valueOf(SurfaceFlingerUtils.getActiveRenderEffect(this)));
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
@@ -254,55 +256,11 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         String val = newValue.toString();
         if (preference == mRenderEffectPref) {
-            writeRenderEffect(Integer.valueOf((String) newValue));
+            int effectId = Integer.valueOf((String) newValue);
+            SurfaceFlingerUtils.setRenderEffect(this, effectId);
             return true;
         }
         return false;
-    }
-
-    // Taken from DevelopmentSettings
-    private void updateFlingerOptions() {
-        // magic communication with surface flinger.
-        try {
-            IBinder flinger = ServiceManager.getService("SurfaceFlinger");
-            if (flinger != null) {
-                Parcel data = Parcel.obtain();
-                Parcel reply = Parcel.obtain();
-                data.writeInterfaceToken("android.ui.ISurfaceComposer");
-                flinger.transact(1010, data, reply, 0);
-                int v;
-                v = reply.readInt();
-                // mShowCpuCB.setChecked(v != 0);
-                v = reply.readInt();
-                // mEnableGLCB.setChecked(v != 0);
-                v = reply.readInt();
-                // mShowUpdatesCB.setChecked(v != 0);
-                v = reply.readInt();
-                // mShowBackgroundCB.setChecked(v != 0);
-
-                v = reply.readInt();
-                mRenderEffectPref.setValue(String.valueOf(v));
-
-                reply.recycle();
-                data.recycle();
-            }
-        } catch (RemoteException ex) {
-        }
-
-    }
-
-    private void writeRenderEffect(int id) {
-        try {
-            IBinder flinger = ServiceManager.getService("SurfaceFlinger");
-            if (flinger != null) {
-                Parcel data = Parcel.obtain();
-                data.writeInterfaceToken("android.ui.ISurfaceComposer");
-                data.writeInt(id);
-                flinger.transact(1014, data, null, 0);
-                data.recycle();
-            }
-        } catch (RemoteException ex) {
-        }
     }
 
     @Override
